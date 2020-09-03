e4157bec56444b1ff102246a32bc4d80a1a2639e
C. Albert Thompson <cthom409@ford.com>
2020-09-03 18:51:03 -0400

    Update error log

--- src/js/readinglist.js
+++ src/js/readinglist.js
@@ -185,8 +185,6 @@ function renderReadingList (readingListEl, animateItems, settings) {
     readingListEl.innerHTML = ''
     setCount(pageList)
     const sortedReadingList = sortReadingList(pageList, settings)
-    console.error(settings)
-    console.error(sortedReadingList)
     const numItems = sortedReadingList.length
 
     // Animate up to 10 items
