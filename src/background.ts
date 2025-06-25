cbedd286cf1e72703e2fd2a21d897a6f1780c3ed
leetcat <casey.thompson001@gmail.com>
2025-06-25 13:16:02 -0400

    Add link click add

--- src/background.ts
+++ src/background.ts
@@ -1,30 +1,31 @@
-import { ListItemData, rl } from './lib/rl';
-import { i18n } from './lib/i18n';
+import {ListItemData, rl} from './lib/rl';
+import {i18n} from './lib/i18n';
 
-// Register the context menu on install
-chrome.runtime.onInstalled.addListener(() => {
+function createContextMenu(id: string, i18nKey: string, defaultTitle: string, contexts: chrome.contextMenus.ContextType[]) {
   chrome.contextMenus.create({
-    id: 'add-to-reading-list',
-    title: i18n.getMessage('addPage', 'Add page to Reading List'),
-    contexts: ['page']
+    id,
+    title: i18n.getMessage(i18nKey, defaultTitle),
+    contexts
+  });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  createContextMenu('add-page-to-reading-list', 'addPage', 'Add page to Reading List', ['page']);
+  createContextMenu('add-link-to-reading-list', 'addLink', 'Add link to Reading List', ['link']);
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  rl.getListItems().then(() => {
+    if (info.menuItemId === 'add-link-to-reading-list' && info.linkUrl) {
+      void addToReadingList(info.linkUrl, info.selectionText || info.linkUrl);
+    } else if (info.menuItemId === 'add-page-to-reading-list' && tab && tab.url) {
+      void addToReadingList(tab.url, tab.title || tab.url);
+    }
   });
 });
 
-// Handle context menu click
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'add-to-reading-list' && tab && tab.url) {
-    const newItem: ListItemData = {
-      url: tab.url,
-      title: tab.title || tab.url,
-      addedAt: Date.now()
-    };
-    rl.getListItems().then(async () => {
-      try {
-        await rl.addReadingItem(newItem);
-      } catch (e) {
-        console.error(e);
-        return;
-      }
-    });
-  }
-});
+async function addToReadingList(url: string, title: string) {
+  console.log(`Adding link to Reading List: ${url}`);
+  const newItem: ListItemData = {url, title, addedAt: Date.now()};
+  await rl.addReadingItem(newItem);
+}
